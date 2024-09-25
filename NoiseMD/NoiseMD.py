ade746024c20b5e6e2ea7bf4685c24b2789f1f37
Gareth Aneurin Tribello <garethtribello@gareths-mbp.mp.qub.ac.uk>
2024-09-25 11:00:55 +0100

    First outline of code

---
+++ NoiseMD/NoiseMD.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class NoiseMD : 
+    def __init__(self) :
+        self.positions = []
+        self.velocities = []
+        self.energy = [] 
+        self.attachments = []
+
+    def create_initial_positions( self, nx, ny ) :
+        """ 
+        This function generates an initial 2D structure which has the atoms on a 2D FCC lattice
+
+        Input arguments: 
+        nx = number of replicas of the unit cell in the x direction
+        ny = number of replicas of the unit cell in the y direction
+        """ 
+        self.positions = ??
+        pass
+
+    def potential( self ) :
+        """
+        Returns:
+        ???
+        """
+        pass
+
+    def runMD( self, nsteps, tstep, temp, friction ) :
+        """ 
+        """
+        print("HELLO WORLD")
+
+    def get_positions( self ) : 
+        return self.positions 
+ 
