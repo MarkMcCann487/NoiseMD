04925ff425db7c30b67063b7714e54b8a77cc4e6
Gareth Aneurin Tribello <garethtribello@gareths-mbp.mp.qub.ac.uk>
2024-09-25 11:37:02 +0100

    Bump version to v0.6.0 for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## [v0.6.0](https://github.com/MarkMcCann487/NoiseMD/releases/tag/v0.6.0) - 2024-09-25
+
 ## [v0.5.0](https://github.com/MarkMcCann487/NoiseMD/releases/tag/v0.5.0) - 2024-09-25
 
 ## [v0.4.0](https://github.com/MarkMcCann487/NoiseMD/releases/tag/v0.4.0) - 2024-09-25
--- NoiseMD/NoiseMD.py
+++ NoiseMD/NoiseMD.py
@@ -1,36 +1,34 @@
 import numpy as np
 
 
-class NoiseMD : 
-    def __init__(self) :
+class NoiseMD:
+    def __init__(self):
         self.positions = np.zeros(10)
         self.velocities = []
-        self.energy = [] 
+        self.energy = []
         self.attachments = []
 
-    def create_initial_positions( self, nx, ny ) :
-        """ 
+    def create_initial_positions(self, nx, ny):
+        """
         This function generates an initial 2D structure which has the atoms on a 2D FCC lattice
 
-        Input arguments: 
+        Input arguments:
         nx = number of replicas of the unit cell in the x direction
         ny = number of replicas of the unit cell in the y direction
-        """ 
+        """
         self.positions = []
         pass
 
-    def potential( self ) :
+    def potential(self):
         """
         Returns:
         ???
         """
         pass
 
-    def runMD( self, nsteps, tstep, temp, friction ) :
-        """ 
-        """
+    def runMD(self, nsteps, tstep, temp, friction):
+        """ """
         print("HELLO WORLD")
 
-    def get_positions( self ) : 
-        return self.positions 
- 
+    def get_positions(self):
+        return self.positions
