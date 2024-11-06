b7e5968e048f26fc4206d7d11d022a74d9e76b21
Mark McCann <40329062@ads.qub.ac.uk>
2024-11-06 14:19:20 +0000

    changed potential function to return potential

--- NoiseMD/NoiseMD.py
+++ NoiseMD/NoiseMD.py
@@ -11,7 +11,8 @@ class NoiseMD:
         self.strides = []
         self.methods = []
 
-    def create_initial_positions(self, nx, ny, a=2**(2/3)):
+    def create_initial_positions(self, nx, ny):
+        a = (2**(2/3))
         self.natoms = 2*nx*ny
         self.positions = np.zeros([self.natoms,2])
 
@@ -37,7 +38,7 @@ class NoiseMD:
 
                 self.forces[i,:] += 4*(self.positions[i,:] - self.positions[j,:])*((12/(r12*r2))-(6/(r2*r6)))
                 self.forces[j,:] += -4*(self.positions[i,:] - self.positions[j,:])*((12/(r12*r2))-(6/(r2*r6)))
-
+        return self.potent
 
     def set_params( self, tstep, temp, friction, nc_const, delx, dely ) : 
         self.tstep = tstep
